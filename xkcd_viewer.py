24d8a1b4b6edea70dee346d0168407e6068a10d3
Ruben Stadler <ruben-stadler@hotmail.com>
2022-08-27 23:29:55 +0200

    xkcd_viewer.py: Added change after time

--- xkcd_viewer.py
+++ xkcd_viewer.py
@@ -4,9 +4,9 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QWidget,
-    QApplication
+    QApplication,
 )
-from PyQt6.QtCore import QThread, pyqtSignal, QObject, Qt
+from PyQt6.QtCore import QThread, pyqtSignal, QObject, Qt, QTimer, QTime
 from PyQt6.QtGui import QPixmap, QResizeEvent
 import sys
 from urllib import request
@@ -14,6 +14,8 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://c.xkcd.com/random/comic/"
+
+timeframes = [QTime(0, 0, 0, 0), QTime(12, 0, 0, 0), QTime(16, 0, 0, 0)]
 
 
 class MainWindow(QMainWindow):
@@ -24,11 +26,13 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         self.pic_pixmap = None
+        self.old_timeframe_idx = 0
+        self.print_string = ""
 
         self.setWindowTitle("xkcd viewer v1.0 by rust")
 
         # Set up button widget
-        self.button = QPushButton("New comic!")
+        self.button = QPushButton("Click for new comic now!")
         self.button.setCheckable(True)
         self.button.clicked.connect(self.downloadThreadStart)
 
@@ -37,15 +41,64 @@ class MainWindow(QMainWindow):
         self.pic.setMinimumSize(1, 1)
         self.pic.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        # Set up text widget
+        self.text = QLabel(self)
+        self.text.setFixedHeight(30)
+        self.text.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Start a timer
+        self.timer = QTimer()
+        self.timer.start(1000)  # Generate signal every 10s
+        self.timer.timeout.connect(self.checkTime)
+
+        self.time = QTime()
+
         # Set up layouting
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.pic)
+        self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
 
         self.widget = QWidget()
         self.widget.setLayout(self.layout)
 
         self.setCentralWidget(self.widget)
+
+    def checkTime(self):
+        no_of_timeframes = len(timeframes)
+        self.current_time = self.time.currentTime()
+
+        for i in range(no_of_timeframes):
+            if self.current_time > timeframes[i]:
+                self.current_timeframe_idx = i
+
+        if self.current_timeframe_idx != self.old_timeframe_idx:
+            self.downloadThreadStart()
+            self.old_timeframe_idx = self.current_timeframe_idx
+
+        self.next_timeframe_idx = (self.current_timeframe_idx + 1) % no_of_timeframes
+        seconds_to_next = self.current_time.secsTo(timeframes[self.next_timeframe_idx])
+
+        if seconds_to_next < 0:
+            seconds_to_next = seconds_to_next + 86400
+
+        self.hours_to_next = seconds_to_next // 3600
+        self.minutes_to_next = ((seconds_to_next % 3600) // 60) + 1
+
+        next_time = timeframes[self.next_timeframe_idx].toString("hh:mm")
+
+        #print(seconds_to_next)
+        print_string = (
+            "Next comic at "
+            + str(next_time)
+            + ". (That's in "
+            + str(self.hours_to_next)
+            + " hour(s) and "
+            + str(self.minutes_to_next)
+            + " minutes.)"
+        )
+
+        self.text.setText(print_string)
 
     def loadImage(self):
         if self.pic_pixmap is not None:
@@ -100,7 +153,7 @@ class DownloadThread(QThread):
             self.worker.deleteLater
         )  # Schedule worker for deletion
         self.finished.connect(self.deleteLater)  # Schedule thread itself for deletion
-        self.finished.connect(lambda: window.button.setText("New comic!"))
+        self.finished.connect(lambda: window.button.setText("Click for new comic now!"))
 
         self.start()  # Start the thread
 
@@ -130,8 +183,6 @@ class DownloadWorker(QObject):
 
         file_tuple = request.urlretrieve(pic_url)
         filepath = file_tuple[0]
-
-        print(f"Temporary filepath: {filepath}")
 
         self.downloadDone.emit(filepath)
 
