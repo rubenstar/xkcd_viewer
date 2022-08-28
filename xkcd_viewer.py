f9757bebf20794010d80f8353546cdd190588e95
Ruben Stadler <ruben-stadler@hotmail.com>
2022-08-28 21:56:28 +0200

    xkcd_viewer.py: Cleanup

--- xkcd_viewer.py
+++ xkcd_viewer.py
@@ -15,118 +15,8 @@ from bs4 import BeautifulSoup
 
 URL = "https://c.xkcd.com/random/comic/"
 
-timeframes = [QTime(0, 0, 0, 0), QTime(12, 0, 0, 0), QTime(16, 0, 0, 0)]
-
-
-class MainWindow(QMainWindow):
-
-    receivedPic = pyqtSignal()
-
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.pic_pixmap = None
-        self.old_timeframe_idx = 0
-        self.print_string = ""
-
-        self.setWindowTitle("xkcd viewer v1.0 by rust")
-
-        # Set up button widget
-        self.button = QPushButton("Click for new comic now!")
-        self.button.setCheckable(True)
-        self.button.clicked.connect(self.downloadThreadStart)
-
-        # Set up center image widget
-        self.pic = QLabel(self)
-        self.pic.setMinimumSize(1, 1)
-        self.pic.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        # Set up text widget
-        self.text = QLabel(self)
-        self.text.setFixedHeight(30)
-        self.text.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        # Start a timer
-        self.timer = QTimer()
-        self.timer.start(1000)  # Generate signal every 10s
-        self.timer.timeout.connect(self.checkTime)
-
-        self.time = QTime()
-
-        # Set up layouting
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.pic)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.widget = QWidget()
-        self.widget.setLayout(self.layout)
-
-        self.setCentralWidget(self.widget)
-
-    def checkTime(self):
-        no_of_timeframes = len(timeframes)
-        self.current_time = self.time.currentTime()
-
-        for i in range(no_of_timeframes):
-            if self.current_time > timeframes[i]:
-                self.current_timeframe_idx = i
-
-        if self.current_timeframe_idx != self.old_timeframe_idx:
-            self.downloadThreadStart()
-            self.old_timeframe_idx = self.current_timeframe_idx
-
-        self.next_timeframe_idx = (self.current_timeframe_idx + 1) % no_of_timeframes
-        seconds_to_next = self.current_time.secsTo(timeframes[self.next_timeframe_idx])
-
-        if seconds_to_next < 0:
-            seconds_to_next = seconds_to_next + 86400
-
-        self.hours_to_next = seconds_to_next // 3600
-        self.minutes_to_next = ((seconds_to_next % 3600) // 60) + 1
-
-        next_time = timeframes[self.next_timeframe_idx].toString("hh:mm")
-
-        #print(seconds_to_next)
-        print_string = (
-            "Next comic at "
-            + str(next_time)
-            + ". (That's in "
-            + str(self.hours_to_next)
-            + " hour(s) and "
-            + str(self.minutes_to_next)
-            + " minutes.)"
-        )
-
-        self.text.setText(print_string)
-
-    def loadImage(self):
-        if self.pic_pixmap is not None:
-            self.pic.setPixmap(
-                self.pic_pixmap.scaled(
-                    self.pic.width(),
-                    self.pic.height(),
-                    Qt.AspectRatioMode.KeepAspectRatio,
-                    Qt.TransformationMode.SmoothTransformation,
-                )
-            )
-
-    def updateImage(self, filepath):
-        self.pic_pixmap = QPixmap(filepath)
-        self.receivedPic.emit()  # Signal back to thread/worker that we're done
-        self.button.setEnabled(True)
-        self.button.setChecked(False)
-
-        self.loadImage()
-
-    def downloadThreadStart(self):
-        self.button.setEnabled(False)
-        self.downloadThread = DownloadThread()
-        self.downloadThread.startThread()
-
-    # Overwrite resizeEvent
-    def resizeEvent(self, a0: QResizeEvent):
-        self.loadImage()
+# Timeframes have to be in order from earliest to latest
+timeframes = [QTime(9, 0, 0, 0), QTime(12, 0, 0, 0), QTime(15, 0, 0, 0)]
 
 
 class DownloadThread(QThread):
@@ -187,10 +77,121 @@ class DownloadWorker(QObject):
         self.downloadDone.emit(filepath)
 
 
+class MainWindow(QMainWindow):
+
+    receivedPic = pyqtSignal()
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.pic_pixmap = None
+        self.old_timeframe_idx = 0
+        self.print_string = ""
+
+        self.setWindowTitle("xkcd viewer v1.0 by rust")
+
+        # Set up button widget
+        self.button = QPushButton("Click for new comic now!")
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.downloadThreadStart)
+
+        # Set up center image widget
+        self.pic = QLabel(self)
+        self.pic.setMinimumSize(1, 1)
+        self.pic.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Set up text widget
+        self.text = QLabel(self)
+        self.text.setFixedHeight(30)
+        self.text.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Start a timer
+        self.timer = QTimer()
+        self.timer.start(10000)  # Generate signal every 10s
+        self.timer.timeout.connect(self.checkTime)
+
+        self.time = QTime()
+
+        # Set up layouting
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.pic)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+
+        self.setCentralWidget(self.widget)
+
+    def checkTime(self):
+        no_of_timeframes = len(timeframes)
+        self.current_time = self.time.currentTime()
+
+        for i in range(no_of_timeframes):
+            if self.current_time > timeframes[i]:
+                self.current_timeframe_idx = i
+
+        if self.current_timeframe_idx != self.old_timeframe_idx:
+            self.downloadThreadStart()
+            self.old_timeframe_idx = self.current_timeframe_idx
+
+        self.next_timeframe_idx = (self.current_timeframe_idx + 1) % no_of_timeframes
+        seconds_to_next = self.current_time.secsTo(timeframes[self.next_timeframe_idx])
+
+        if seconds_to_next < 0:
+            seconds_to_next = seconds_to_next + 86400
+
+        self.hours_to_next = seconds_to_next // 3600
+        self.minutes_to_next = ((seconds_to_next % 3600) // 60) + 1
+
+        next_time = timeframes[self.next_timeframe_idx].toString("hh:mm")
+
+        print_string = (
+            "Next comic at "
+            + str(next_time)
+            + ". (That's in "
+            + str(self.hours_to_next)
+            + " hour(s) and "
+            + str(self.minutes_to_next)
+            + " minute(s).)"
+        )
+
+        self.text.setText(print_string)
+
+    def loadImage(self):
+        if self.pic_pixmap is not None:
+            self.pic.setPixmap(
+                self.pic_pixmap.scaled(
+                    self.pic.width(),
+                    self.pic.height(),
+                    Qt.AspectRatioMode.KeepAspectRatio,
+                    Qt.TransformationMode.SmoothTransformation,
+                )
+            )
+
+    def updateImage(self, filepath):
+        self.pic_pixmap = QPixmap(filepath)
+        self.receivedPic.emit()  # Signal back to thread/worker that we're done
+        self.button.setEnabled(True)
+        self.button.setChecked(False)
+
+        self.loadImage()
+
+    def downloadThreadStart(self):
+        self.button.setEnabled(False)
+        self.downloadThread = DownloadThread()
+        self.downloadThread.startThread()
+
+    # Overwrite resizeEvent
+    def resizeEvent(self, a0: QResizeEvent):
+        self.loadImage()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     window = MainWindow()
     window.show()
+    window.checkTime()
 
     app.exec()
